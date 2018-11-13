6dc3e475880169b86674a46bbcd3efcb26204c83
Jakša Bašić <jaksabasic1@gmail.com>
2018-11-13 12:45:12 +0100

    chore: cleanup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,39 @@
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react';
+import { TradeStore, RouterStore } from '../stores';
 import {
   STORE_TRADE,
   STORE_ROUTER,
 } from '../constants';
 
-@inject(STORE_TRADE, STORE_ROUTER)
-@observer
-export default class Home extends Component {
+interface HomeProps {
+  /** MobX Stores will be injected via @inject() **/
+  [STORE_ROUTER]: RouterStore,
+  [STORE_TRADE]: TradeStore;
+}
+
+interface HomeState {}
+
+class Home extends Component<HomeProps, HomeState> {
+  constructor(props: HomeProps) {
+    super(props);
+  }
+  getData = () => {
+    const tradeStore = this.props[STORE_TRADE] as TradeStore;
+    tradeStore.getData();
+  }
   render() {
     console.log(this.props)
     return (
       <div>
         Home
+        <button onClick={this.getData}>getData</button>
       </div>
     )
   }
 }
+
+export default inject(
+  STORE_TRADE,
+  STORE_ROUTER
+)(observer(Home));
--- src/stores/TradeStore.ts
+++ src/stores/TradeStore.ts
@@ -1,16 +1,26 @@
-import { observable, computed, action } from 'mobx';
+import { observable, computed, action, flow } from 'mobx';
 import { TradeModel } from '../models/TradeModel';
+
+interface Order {
+  BTC: number,
+  USD: number,
+  EUR: number
+}
 
 export class TradeStore {
   constructor(fixtures: TradeModel[]) {
     this.trades = fixtures;
     this.tradeHistory = fixtures;
-    this.tradeOrders = fixtures;
+    this.tradeOrders = {
+      BTC: 0.5,
+      USD: 215,
+      EUR: 185
+    };
   }
 
   @observable public trades: Array<TradeModel>;
   @observable public tradeHistory: Array<TradeModel>;
-  @observable public tradeOrders: Array<TradeModel>;
+  @observable public tradeOrders: Order;
 
   @computed
   get activeTrades() {
@@ -21,6 +31,29 @@ export class TradeStore {
   addTrade = (item: TradeModel): void => {
     this.trades.push(new TradeModel(item.text, item.completed));
   };
+
+  @action
+  async getData() {
+    try {
+      const res = await fetch('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR');
+      const data = await res.json();
+      this.tradeOrders = data;
+      console.log(this.tradeOrders)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  @action
+  public loadTags = flow(function* (this: TradeStore) {
+    try {
+      const res = yield fetch('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=BTC,USD,EUR');
+      const data = yield res.json();
+      this.tradeOrders = data;
+    } catch (err) {
+      console.log(err)
+    }
+  })
 }
 
 export default TradeStore;
